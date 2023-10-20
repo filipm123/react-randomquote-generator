34158775fbc210fc7bc47c870b5618e3594a137a
Filipor <filipor123@gmail.com>
2023-10-20 14:30:45 +0200

    fixed moving button

--- src/App.jsx
+++ src/App.jsx
@@ -31,12 +31,13 @@ function Card() {
   function handleClick() {
     setQuoteIndex(Math.floor(quotes.length * Math.random()));
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  
     document.body.style.backgroundColor = "#" + randomColor;
   }
 
   return (
     <div id="quote-box">
-      <div id="text">"{displayQuote}"</div>
+      <div id="text"><i>"{displayQuote}"</i></div>
 
       <div id="author">-{displayAuthor}</div>
 
